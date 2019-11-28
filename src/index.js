37bff334f54eddf08ffd4025325fecbe114c8bce
Adarsh Chaurasia <nextgen.adarsh@Adarshs-MacBook-Air.local>
2019-11-28 20:41:23 +0530

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import HtmlComponent from './components/html-component'
+import PrintMe from './print-me';
 
-document.body.appendChild(HtmlComponent());
+const ele = HtmlComponent();
 
-export {default as HtmlComponent} from './components/html-component';
+const btn = document.createElement('button');
+btn.innerHTML = 'Click me and check the console!';
+btn.onclick = PrintMe;
+
+ele.appendChild(btn);
+
+// Render elements
+document.body.appendChild(ele);
+
+
+export { default as HtmlComponent } from './components/html-component';
